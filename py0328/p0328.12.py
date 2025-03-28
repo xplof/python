e08e41ec4d88081b01637877ce9b3b1d9760efe4
xplof <ak4713579@naver.com>
2025-03-28 18:24:23 +0900

    수업

--- py0328/p0328.12.py
+++ py0328/p0328.12.py
@@ -1,4 +1,25 @@
+# while True:
+#     num=int(input("숫자를 입력하세요"))
+#     if num==0:
+#         break
+
 while True:
-    num=int(input("숫자를 입력하세요"))
-    if num==0:
+    print("[ 프로그램 구현 ]")
+    print("-"*33)
+    print("1. 숫자맞추기")
+    print("2. 로또맞추기")
+    print("3. 학생성적프로그램")
+    print("0. 종료")
+    print("-"*33)
+    choice=int(input("원하는 번호를 입력하세요 : "))
+    
+    if choice==1:
+        print("[숫자맞추기 프로그램]")
+    elif choice==2:
+        print("[로또맞추기 프로그램]")
+    elif choice==3:
+        print("[학생성적 프로그램]")
+    elif choice==0:
+        print("[프로그램 종료]")
         break
+        
